588a7f7884e1ccc511f12793df5fe6e80da11615
lxhom <73352734+lxhom@users.noreply.github.com>
2021-07-16 22:46:59 +0200

    Update share2youtubedl.sh

--- share2youtubedl.sh
+++ share2youtubedl.sh
@@ -4,19 +4,19 @@ clear
 set -x
 if [[ "$1" =~ ^.*youtu.*$ ]] || [[ "$1" =~ ^.*youtube.*$ ]]; then
   echo -e "Downloading video...\\n>URL: ${1}\\n"
-  youtube-dl -F "$1"
+  youtube-dl -F "\"$1\""
   read -p "Choose your video quality (sd, hd or best): " video
 
   if [[ "$video" = "sd" ]]; then
     video="18"
   fi
   if [[ "$video" = "hd" ]]; then
-    audio="22"
+    video="22"
   fi
-  youtube-dl -f "$video" "$1"
+  youtube-dl -f "$video" "\"$1\""
 elif [[ "$1" =~ ^.*nourlselected.*$ ]]; then
   echo "There was an error"
-else youtube-dl -f best "$1"
+else youtube-dl -f best "\"$1\""
 fi
 
 read -p "Press enter to continue"
